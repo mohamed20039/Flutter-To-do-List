5e2e782661516d22b10f18052f3fbc6500f1a39e
Mohamed <mohammedmohamuud.01@gmail.com>
2024-02-24 20:39:59 +0300

    Complete the Search bar

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:to_do_list/Constants/colors.dart';
 
@@ -9,7 +11,31 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
-      body: Container(child: Text("Hello World!")),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+        child: Column(children: [SearchBox()]),
+      ),
+    );
+  }
+
+  Widget SearchBox() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
+      child: TextField(
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(0),
+              prefixIcon: Icon(
+                Icons.search,
+                color: tdBlack,
+                size: 20,
+              ),
+              prefixIconConstraints:
+                  BoxConstraints(maxHeight: 20, minWidth: 20),
+              border: InputBorder.none,
+              hintText: "Search",
+              hintStyle: TextStyle(color: tdGrey))),
     );
   }
 
